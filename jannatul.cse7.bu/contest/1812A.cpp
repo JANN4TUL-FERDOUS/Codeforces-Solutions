cdcb3569f7c835b15518d60e05f382b3903c993d
GitHub Action <action@github.com>
2023-04-02 02:22:50 +0000

    CF-AC-Submissions

---
+++ jannatul.cse7.bu/contest/1812A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=3e5+7;
+//ll b[N],a[N];
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+  	cout<<"security\n";
+	return 0;
+}
---
+++ jannatul.cse7.bu/contest/1812E.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=3e5+7;
+//ll b[N],a[N];
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+  	ll x,y,z;
+  	cin>>x>>y>>z;
+  	cout<<"0\n";
+	return 0;
+}
