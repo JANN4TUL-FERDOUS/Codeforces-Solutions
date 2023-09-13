aee6104313a8e4f17a38f43cb00c5f8a15187cd6
GitHub Action <action@github.com>
2023-09-13 00:58:42 +0000

    CF-AC-Submissions

---
+++ jannatul.cse7.bu/contest/1867A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+
+const int N=1e5+7;
+ll mod=1e9+7;
+ll ax[8]={0,0,-1,+1,-1,-1,1,1};
+ll ay[8]={-1,1,0,0,-1,1,-1,1};
+ll kx[8]={2,2,-2,-2,1,1,-1,-1};
+ll ky[8]={1,-1,1,-1,2,-2,2,-2};
+ll back[N+1],frow[N+1];
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t=1;
+    cin>>t;
+    ll tt=t;
+    while(t--){
+		ll n,m,k,i,j=-1,s=0,s2=0,s3=0,z=0,z2=0;
+		cin>>n;
+		pair<ll,ll>p[n];
+		ll a[n];
+		for(i=0;i<n;i++) {cin>>s;p[i]={s,i};}
+		sort(p,p+n);
+		for(i=0;i<n;i++) a[p[i].ss]=n-i;
+		for(i=0;i<n;i++) cout<<a[i]<<" ";
+		cout<<"\n";
+		
+	}
+	return 0;
+}
